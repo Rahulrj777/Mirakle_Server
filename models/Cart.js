c3b725152b02ee43ac67e540486dc54e0a446dd1
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-05 18:51:14 +0530

    Force redeploy with vercel.json update

--- models/Cart.js
+++ models/Cart.js
@@ -1,18 +1,26 @@
-// models/Cart.js
-import mongoose from 'mongoose';
+router.post('/', authMiddleware, async (req, res) => {
+  const { items } = req.body;
+  const userId = req.user.userId;
 
-const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  items: [
-    {
-      _id: String, // Product ID
-      title: String,
-      images: Object,
-      weight: Object,
-      currentPrice: Number,
-      quantity: Number,
+  let cart = await Cart.findOne({ userId });
+
+  if (!cart) {
+    // Create new cart
+    cart = new Cart({ userId, items });
+  } else {
+    for (const newItem of items) {
+      const index = cart.items.findIndex(i => i._id === newItem._id);
+
+      if (index !== -1) {
+        // Item exists: increase quantity
+        cart.items[index].quantity += newItem.quantity || 1;
+      } else {
+        // Add new item
+        cart.items.push({ ...newItem, quantity: newItem.quantity || 1 });
+      }
     }
-  ],
-}, { timestamps: true });
+  }
 
-export default mongoose.model('Cart', cartSchema);
+  await cart.save();
+  res.json({ message: 'Cart updated', cart });
+});
