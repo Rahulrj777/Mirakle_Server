0642ea7951d87ca2c5d05018eb1af175697f052d
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-09 06:24:07 +0530

    Force redeploy with vercel.json update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,7 +4,6 @@ import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 import Product from '../models/Product.js';
-import { Product } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
