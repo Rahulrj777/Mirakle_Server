225dd30790eaec9de8c2f7fe30cad913be33fbdc
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-18 10:19:32 +0530

    Force redeploy with vercel.json update

--- models/OfferBanner.js
+++ models/OfferBanner.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 
 const offerBannerSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  percentage: { type: String }, // Optional
   imageUrl: { type: String, required: true },
   slot: { type: String, enum: ["left", "right"], required: true }, 
 }, { timestamps: true });
