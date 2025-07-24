86686242169329b58e57c1f1d1e9ed2f0ec79f5e
Rahul Srinivasan J <rahulsrinivasannkl@gmail.com>
2025-07-24 16:22:36 +0530

    Initial commit from new laptop

--- models/Cart.js
+++ models/Cart.js
@@ -1,25 +1,35 @@
-// models/user.js
 import mongoose from "mongoose"
 
-const addressSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
-  line1: String,
-  city: String,
-  pincode: String,
-  landmark: String,
-  type: { type: String, default: "HOME" },
-}, { _id: true }); // _id is used for delete/edit
+const itemSchema = new mongoose.Schema(
+  {
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true }, 
+    variantId: { type: mongoose.Schema.Types.ObjectId },
+    title: { type: String, required: true },
+    images: {
+      others: [{ type: String }],
+    },
+    size: { type: String },
+    weight: {
+      value: { type: mongoose.Schema.Types.Mixed },
+      unit: { type: String },
+    },
+    currentPrice: { type: Number, required: true },
+    quantity: { type: Number, required: true, default: 1 },
+  },
+  { _id: false },
+)
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String, required: true, minlength: 6 },
-  isAdmin: { type: Boolean, default: false },
-  addresses: [addressSchema], // 🔥 Add this line
-}, {
-  timestamps: true,
-});
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [itemSchema],
+  },
+  { timestamps: true },
+)
 
-
-export default mongoose.model("User", userSchema)
+export default mongoose.model("Cart", CartSchema)
